3636f6cb991fdf143cdd7f38eeb01f8cbd44619d
Alex <golovachalex@gmail.com>
2022-10-12 16:34:40 +0200

    Update setup_ubuntu.sh
    fix zsh install

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install curl git htop mc vim
+sudo apt update
+sudo apt install -y curl git htop mc vim
 
 #install ZSH
-sudo apt install zsh
-sudo apt-get install fonts-powerline
+sudo apt install -y zsh
+sudo apt install -y fonts-powerline
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-
-# zsh plugins and configuration
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-sed "s/^ZSH_THEME=.*/ZSH_THEME=\"agnoster\"/g" ~/.zshrc 
-sed "s/^plugins=/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g" ~/.zshrc
-sed "1i DEFAULT_USER=\"\$USER\"" ~/.zshrc
+sed -i "s/^ZSH_THEME=.*/ZSH_THEME=\"agnoster\"/g" ~/.zshrc 
+sed -i "s/^plugins=.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g" ~/.zshrc
+sed -i "1i DEFAULT_USER=\"\$USER\"" ~/.zshrc
 
 sudo chsh -s /usr/bin/zsh
